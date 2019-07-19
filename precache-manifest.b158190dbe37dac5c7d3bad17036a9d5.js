d47783a400240578d0dd85c720519e6f087d3b99
Faisal <faisal@rotatestudios.com>
2019-07-19 17:04:34 +0100

    Updates

---
+++ precache-manifest.b158190dbe37dac5c7d3bad17036a9d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fc6064d9a1741c16693",
+    "url": "/trello-clone/static/css/main.5eac69f3.chunk.css"
+  },
+  {
+    "revision": "0fc6064d9a1741c16693",
+    "url": "/trello-clone/static/js/main.0fc6064d.chunk.js"
+  },
+  {
+    "revision": "77a8e5d759d4dcebcb23",
+    "url": "/trello-clone/static/js/runtime~main.77a8e5d7.js"
+  },
+  {
+    "revision": "4f3c0837610a35ae087d",
+    "url": "/trello-clone/static/js/2.4f3c0837.chunk.js"
+  },
+  {
+    "revision": "e1027338ac7be8ebae7d04a5bed9ae49",
+    "url": "/trello-clone/index.html"
+  }
+];
